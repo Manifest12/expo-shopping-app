c4dd0218814d5c9a4c39feea243d77d895b3224d
Manifest12 <sdamptey17@gmail.com>
2021-11-04 02:20:50 +0000

    No comment

---
+++ screens/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function homepage({navigation}) {
+    return(
+
+        <View>
+            <Text> This is the homepage </Text>
+        </View>
+
+    );
+}
