99da52b9bcb60c378439de7ef5c72313a9827713
Manifest12 <sdamptey17@gmail.com>
2021-11-04 03:41:27 +0000

    Another commit

--- screens/cart.js
+++ screens/cart.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import { View, Text} from 'react-native';
+
+export default function cart(){
+
+    return(
+        <View>
+            <Text> This is the cart! </Text>
+        </View>
+    );
+}
--- screens/login.js
+++ screens/login.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput, Button, TouchableHighlight } from 'react-native';
 
 export default function homepage({navigation}) {
     return(
+        <View style={{backgroundColor: '#ffffff'}}>
+            <View style={{alignItems: 'center', justifyContent: 'center', }}>
+                <Text style={{
+                    fontSize: 30, fontWeight: 'bold', textShadowColor: '#888888',
+                    textShadowOffset: {height: 10, width: 10}
+                    }}> Manifest Stores </Text>
+            </View>
 
-        <View>
-            <Text> This is the homepage </Text>
+            <View>
+                <TextInput placeholder='Email/Username'
+                style={{width: 150, height: 90}}/>
+                <TextInput placeholder='Password' 
+                style={{width:150, height: 90, marginBottom: 20}}/>
+                <Button 
+                onPress={() => navigation.navigate('home')}
+                title='Login'
+                />
+            </View>
+
+            <View>
+                <Text> No account yet? </Text>
+                <TouchableHighlight onPress={() => alert('In Progress')}>
+                    <Text> Register Here </Text>
+                </TouchableHighlight>
+            </View>
         </View>
-
+        
     );
 }
